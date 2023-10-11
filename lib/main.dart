b76da3015566d0da37078077f6e90e6676470a43
Solonirina Niary Razafinimpanana <niary.razafi@pulse.mg>
2023-10-11 09:42:55 +0300

    Calculatrice V1.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 void main(){
   runApp(Calculatrice());
@@ -26,12 +27,48 @@ class CalculatriceSimple extends StatefulWidget {
 
 class _CalculatriceSimpleState extends State<CalculatriceSimple> {
 
+  String equation = "0";
+  String result = "0";
+  String expression = "0";
+
+  ButtonPressed(String textButton) {
+    // print(textButton);
+    setState(() {
+      if(textButton == "C"){
+        equation = "0";
+        result = "0";
+      } else if (textButton == "⌫"){
+        equation = equation.substring(0, equation.length - 1);
+        if (equation.isEmpty){
+          equation = "0";
+        }
+      } else if (textButton == "="){ //Expression mathématique pour faire les calculs
+          expression = equation;
+          try{
+            Parser parser = Parser();
+            Expression exp = parser.parse(expression);
+            ContextModel cm = ContextModel();
+            result = "${exp.evaluate(EvaluationType.REAL, cm)}";
+          }catch(e){
+            result = "Syntax error";
+            print(e);
+          }
+      } else{
+        if (equation == "0"){
+          equation = textButton;
+        }else{
+          equation = equation + textButton;
+        }
+      }
+    });
+  }
+
   Widget calculatriceButton(String textButton, Color colorText, Color colorButton){
     return Container(
       height: MediaQuery.of(context).size.height * 0.1,
       padding: EdgeInsets.all(16),
       color: colorButton,
-      child: MaterialButton(onPressed: null, child: Text(textButton, style: TextStyle(color: colorText, fontSize: 30, fontWeight: FontWeight.normal),),),
+      child: MaterialButton(onPressed: ()=>ButtonPressed(textButton), child: Text(textButton, style: TextStyle(color: colorText, fontSize: 30, fontWeight: FontWeight.normal),),),
     );
   }
 
@@ -44,32 +81,60 @@ class _CalculatriceSimpleState extends State<CalculatriceSimple> {
       ),
       body: Column(
         children: [
-          Container( //Conteneur qui contient l'équation
+          Container( //Contient l'équation saisie par l'utilisateur
             alignment: Alignment.centerRight,
             padding: const EdgeInsets.fromLTRB(20, 10, 10, 0),
-            child: Text('0', style: TextStyle(fontSize: 35),),
+            child: Text(equation, style: TextStyle(fontSize: 35),),
           ),
-          Container( //Conteneur qui contient les chiffres saisis par l'utilisateur
+          Container( //Contient les chiffres saisis par l'utilisateur ainsi que le résultat obtenu après avoir validé l'équation
             alignment: Alignment.centerRight,
             padding: const EdgeInsets.fromLTRB(20, 10, 10, 0),
-            child: Text('0', style: TextStyle(fontSize: 35),),
+            child: Text(result, style: TextStyle(fontSize: 35),),
           ),
           const Expanded(child: Divider()),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
+              Container( //Contient le pad de la calculatrice
                 width: MediaQuery.of(context).size.width,
                 child: Table(
                 children: [
                   TableRow(
                     children: [
                       calculatriceButton("C", Colors.red, Colors.white),
-                      MaterialButton(onPressed: null, child: Text('⌫', style: TextStyle(color: Colors.red),),),
-                      MaterialButton(onPressed: null, child: Text('%', style: TextStyle(color: Colors.red),),),
-                      MaterialButton(onPressed: null, child: Text('+', style: TextStyle(color: Colors.red),),)
+                      calculatriceButton("⌫", Colors.red, Colors.white),
+                      calculatriceButton("%", Colors.red, Colors.white),
+                      calculatriceButton("/", Colors.red, Colors.white),
                     ],
-                  )
+                  ),TableRow(
+                    children: [
+                      calculatriceButton("7", Colors.black, Colors.white),
+                      calculatriceButton("8", Colors.black, Colors.white),
+                      calculatriceButton("9", Colors.black, Colors.white),
+                      calculatriceButton("*", Colors.red, Colors.white),
+                    ],
+                  ),TableRow(
+                    children: [
+                      calculatriceButton("4", Colors.black, Colors.white),
+                      calculatriceButton("5", Colors.black, Colors.white),
+                      calculatriceButton("6", Colors.black, Colors.white),
+                      calculatriceButton("-", Colors.red, Colors.white),
+                    ],
+                  ),TableRow(
+                    children: [
+                      calculatriceButton("1", Colors.black, Colors.white),
+                      calculatriceButton("2", Colors.black, Colors.white),
+                      calculatriceButton("3", Colors.black, Colors.white),
+                      calculatriceButton("+", Colors.red, Colors.white),
+                    ],
+                  ),TableRow(
+                    children: [
+                      calculatriceButton(".", Colors.black, Colors.white),
+                      calculatriceButton("0", Colors.black, Colors.white),
+                      calculatriceButton("00", Colors.black, Colors.white),
+                      calculatriceButton("=", Colors.red, Colors.white),
+                    ],
+                  ),
                 ],
               ),)
             ],
